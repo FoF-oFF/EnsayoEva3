a857bba11eebfb2384656eb9a94ff5a9aba1b0d3
FoF-oFF <jef.saavedra@duocuc.cl>
2026-05-25 21:36:44 -0400

    Add files via upload

---
+++ Bicicletas.py
@@ -0,0 +1,56 @@
+print("Bienvenido al sistema de gestion de bicicletas")
+capacidad_maxima = 25
+bicis_disponibles = 25
+viajes_activos = 0
+ejecutando = True
+
+#ciclo principal
+while ejecutando:
+    print("\n=== MENÚ PRINCIPAL ===")
+    print("1.Bicicletas disponibles")
+    print("2.Arrendar bicicletas (salida)")
+    print("3.Devolver bicicletas (entrada)")
+    print("4.Historial de viajes activos")
+    print("5.Salir")
+    try:
+        opcion = int(input("Selecciona una opción"))
+    except ValueError:
+        print("Opcion no valida")
+        continue
+    #opcion 1
+    if opcion == 1:
+        print(f"\n[INFO] Cantidad actual de bicicletas disponibles: {bicis_disponibles}")
+    #opcion 2
+    elif opcion == 2:
+        print(f"\n--- Arrendar bicicletas (Disponibles: {bicis_disponibles})---")
+        if bicis_disponibles == 0:
+            print("Lo sentimos, no quedan bicicletas disponibles")
+        else:
+            try:
+                cantidad_a_arrendar = int(input("¿Cuantas bicicletas desea arrendar?: "))
+                if cantidad_a_arrendar <= 0:
+                    print("Error: la cantidad a arrendar debe ser mayor a 0")
+                elif cantidad_a_arrendar > bicis_disponibles:
+                    print(f"No hay suficientes bicicletas, puede arrendar hasta: {bicis_disponibles}")
+                else:
+                    bicis_disponibles -= cantidad_a_arrendar
+                    viajes_activos += cantidad_a_arrendar
+                    print(f"Arriendo exitoso, ha retirado {cantidad_a_arrendar} bicis")
+            except ValueError:
+                print("Error, debe ingresar un número entero")
+    #opcion 3
+    elif opcion == 3:
+        diferencia = capacidad_maxima - bicis_disponibles
+        print(f"\n--- Devolver bicicleta (espacio libre en estación: {diferencia})")
+        try: 
+            cantidad_a_devolver = int(input("¿Cuantas bicicletas desea devolver?: "))
+            if cantidad_a_devolver <= 0:
+                print("Error: la cantidad a devolver debe ser mayor a 0")
+            elif bicis_disponibles + cantidad_a_devolver > capacidad_maxima:
+                print("Error: Supera la capacidad maxima d bicicletas")
+            else:
+                bicis_disponibles += cantidad_a_devolver
+                viajes_activos -= cantidad_a_devolver
+                print(f"Devolucion exitosa ha regresado {cantidad_a_devolver} bicis")
+        except ValueError:
+            print("Error: debe ingresar un numero entero valido")
